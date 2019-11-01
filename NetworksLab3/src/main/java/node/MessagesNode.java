faebfc648f20a4bb527ed239af14b47f99afb018
Meg-geek <m.khlybova@g.nsu.ru>
2019-11-01 17:01:32 +0700

    sendMessages to all connected nodes

--- NetworksLab3/src/main/java/node/MessagesNode.java
+++ NetworksLab3/src/main/java/node/MessagesNode.java
@@ -1,11 +1,10 @@
 package node;
 
-import node.nodeMessages.NodeMessage;
-
 import java.util.List;
 
 public interface MessagesNode extends Node {
     void sendMessage(NodeMessage message, NodeInfo nodeInfo);
     void sendMessage(NodeMessage message, List<NodeInfo> nodesInfoList);
+    void sendMessage(NodeMessage message);
     void addRecvMessage(NodeMessage message);
 }
--- NetworksLab3/src/main/java/node/Node.java
+++ NetworksLab3/src/main/java/node/Node.java
@@ -1,9 +1,5 @@
 package node;
 
-import node.nodeMessages.NodeMessage;
-
-import java.util.List;
-
 public interface Node {
     int TIMEOUT_MILSEC = 3000;
     int MESSAGE_CAPACITY = 100;
