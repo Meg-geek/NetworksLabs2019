a3a7926d87630dfd1f676cd4ffbe39336b77e18b
Meg-geek <m.khlybova@g.nsu.ru>
2019-11-22 14:13:02 +0700

    made abstract class instead of interface

--- netlab4/src/main/java/model/networkUtils/Message.java
+++ netlab4/src/main/java/model/networkUtils/Message.java
@@ -1,8 +1,28 @@
 package model.networkUtils;
 
-public interface Message {
-    long getNumber();
-    int getSenderID();
-    int getReceiverID();
-    MessageType getType();
+public abstract class Message {
+    private long messageNumber;
+    private int recieverID, senderID;
+
+    public Message(){}
+
+    public Message(long messageNumber, int senderID, int recieverID){
+        this.messageNumber = messageNumber;
+        this.senderID = senderID;
+        this.recieverID = recieverID;
+    }
+
+    final public long getNumber(){
+        return messageNumber;
+    }
+
+    final public int getSenderID(){
+        return senderID;
+    }
+
+    final public int getReceiverID(){
+        return recieverID;
+    }
+
+    abstract public MessageType getType();
 }
