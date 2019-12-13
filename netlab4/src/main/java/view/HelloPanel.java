92268693f74af113a642fe477589f2c3b25acb61
Meg-geek <m.khlybova@g.nsu.ru>
2019-12-13 15:47:56 +0700

    added Hello text

--- netlab4/src/main/java/view/HelloPanel.java
+++ netlab4/src/main/java/view/HelloPanel.java
@@ -7,25 +7,36 @@ import java.awt.event.MouseEvent;
 
 class HelloPanel extends JPanel {
     private JTextField nameField;
-    private JLabel textLabel;//, helloTextLabel;
+    private JLabel textLabel, helloTextLabel;
     private JButton startGameButton;
     private static final int COLUMNS = 50;
     private static final String FIELD_DESCR = "Введите свое имя";
     private static final String BUTTON_TEXT = "Начать";
-   // private static final String HELLO_TEXT = "Добро пожаловать в сетевую игру змейка!";
+    private static final String HELLO_TEXT = "Добро пожаловать в сетевую игру змейка!";
     //private static final int COLUMNS_AMOUNT = 1;
     //private static final int ROWS_AMOUNT = 3;
+    private final int HELLO_FONT_SIZE = SnakeGameFrame.FRAME_HEIGHT/20;
+    private final int TEXT_FONT_SIZE = SnakeGameFrame.FRAME_HEIGHT/22;
 
     HelloPanel(SnakeGameFrame gameFrame){
         textLabel = new JLabel(FIELD_DESCR);
-       // helloTextLabel = new JLabel(HELLO_TEXT);
+        textLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, TEXT_FONT_SIZE));
+        helloTextLabel = new JLabel(HELLO_TEXT);
+        helloTextLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, HELLO_FONT_SIZE));
         nameField = new JTextField(COLUMNS);
         startGameButton  = new JButton(BUTTON_TEXT);
         setBackground(SnakeGameFrame.BACKGROUND_COLOR);
         //setLayout(new GridLayout(ROWS_AMOUNT, COLUMNS_AMOUNT));
-       // add(helloTextLabel);
-        add(textLabel, BorderLayout.NORTH);
-        add(nameField, BorderLayout.CENTER);
+        JPanel textPanel = new JPanel(){
+            {
+                setBackground(SnakeGameFrame.BACKGROUND_COLOR);
+            }
+        };
+        textPanel.setLayout(new GridLayout(2,1));
+        textPanel.add(helloTextLabel);
+        textPanel.add(textLabel);
+        add(textPanel, BorderLayout.NORTH);
+        add(nameField, BorderLayout.SOUTH);
         add(startGameButton, BorderLayout.CENTER);
         setButtonSettings(gameFrame);
     }
